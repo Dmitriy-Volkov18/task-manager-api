1493f44c225ee561029458f24d866de1a09085a3
Dima Vokov <dima.dima99.vokov@mail.ru>
2020-01-20 01:53:40 +0200

    Init commit

---
+++ src/routers/task.js
@@ -0,0 +1,147 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+const multer = require('multer')
+
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
+    try {
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({'error': 'Invalid updates'})
+    }
+    
+    try {
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
+        if(!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(file.originalname.match('/\.(jpg|jpeg|png)$/')){
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
+    }
+})
+
+module.exports = router
